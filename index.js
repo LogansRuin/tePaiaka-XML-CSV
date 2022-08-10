6de40c00e0b5464abce34b3dfb9481f9b7c7724c
Jared <jared@needapart.co.nz>
2022-08-10 14:12:11 +1200

    findChilcren returns an array of child category ids

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ const app = 'Hello world'
 
 // convert xml to js object
 // for the moment just going to start with an existing json
-const categories = require('./data/categories.json').category
+const categories = require('./data/categories.json').category[0]
+
+// find children for a category
+function findChildren (id, arr) {
+  const children = []
+  arr.forEach(category => {
+    if (category.parent == id) {
+      children.push(category.categoryId)
+    }
+  })
+  return children
+}
+
+// add children key to each and fill with direct decendants of the tree
 
 // set up for csv with rows array
 
 // write csv file to data folder
 
-module.exports = { app }
+module.exports = { app, findChildren }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,35 @@
-const app = require('./index').app
+const app = require('./index')
 
 test('Tests are working', () => {
   expect(false).toBeFalsy()
+})
+
+describe('findChildren', () => {
+  const arr = [
+    {
+      categoryId: 1001,
+      parent: 'root'
+    },
+    {
+      categoryId: 1002,
+      parent: '1001'
+    },
+    {
+      categoryId: 1003,
+      parent: '1001'
+    },
+    {
+      categoryId: 1004,
+      parent: '1002'
+    },
+    {
+      categoryId: 1005,
+      parent: '1001'
+    }
+  ]
+  test('returns an array', () => {
+    expect(app.findChildren(1001, arr)).toEqual(expect.arrayContaining([1002, 1003, 1005]))
+    expect(app.findChildren(1001, arr)).not.toEqual(expect.arrayContaining([1001, 1004]))
+    expect(app.findChildren(1001, arr).length).toBe(3)
+  })
 })
