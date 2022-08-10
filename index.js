363c109b8e055ab510572853abea678d276f2764
Jared <jared@needapart.co.nz>
2022-08-10 13:03:17 +1200

    get category objects into an array

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
 const app = 'Hello world'
+
+// read xml
+
+// convert xml to js object
+// for the moment just going to start with an existing json
+const categories = require('./data/categories.json').category
+
+// set up for csv with rows array
+
+// write csv file to data folder
 
 module.exports = { app }
