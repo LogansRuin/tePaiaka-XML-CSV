c1a3c80c145e6ac80931d5b5450e343178422e40
Jared <jared@needapart.co.nz>
2022-08-10 23:34:53 +1200

    buildCsvData turns an array objects in to an array of CSV data

--- index.js
+++ index.js
@@ -28,8 +28,49 @@ function insertChildren (arr) {
   })
   return arr
 }
+
 // set up for csv with rows array
+function buildCsvData (obj, arr, rows = [['name', 'id', 'level 1', 'level 2', 'level 3', 'level 4']], treeDepth = 0) {
+  if (obj.children.length > 0) {
+    const children = obj.children
+
+    children.forEach(childId => {
+      // find object in array with child as the categoryID
+      const child = arr.find(e => e.categoryId == childId)
+
+      if (rows.length > 1) {
+        // make copy of parent row
+        const row = JSON.parse(JSON.stringify(rows.find(e => e[1] == child.parent)))
+        // replace name and categoryId with child values
+        row[0] = child['display-name']
+        row[1] = child.categoryId
+        // add child category id to the end of array
+        row.push(child.categoryId)
+        // add child row to the array
+        rows.push(row)
+      } else {
+        // create first row of data
+        const row = [child['display-name'], child.categoryId]
+        row[2] = child.parent
+        row[3] = child.categoryId
+        // add row to the array
+        rows.push(row)
+      }
+
+      buildCsvData(child, arr, rows, treeDepth)
+    })
+  } else {
+    treeDepth = 0
+  }
+  // start with the chidren of root and build out new arr
+  console.log(rows)
+  return rows
+}
+
+// // add and populate children property to categories
+// const rows = []
+// const categories = insertChildren(arr)
 
 // write csv file to data folder
 
-module.exports = { app, findChildren, insertChildren }
+module.exports = { app, findChildren, insertChildren, buildCsvData }
--- index.test.js
+++ index.test.js
@@ -1,11 +1,7 @@
 const app = require('./index')
 
-test('Tests are working', () => {
-  expect(false).toBeFalsy()
-})
-
 describe('findChildren', () => {
-  const arr = [
+  const data = [
     {
       categoryId: 1001,
       parent: 'root'
@@ -28,14 +24,14 @@ describe('findChildren', () => {
     }
   ]
   test('returns an array', () => {
-    expect(app.findChildren(1001, arr)).toEqual(expect.arrayContaining([1002, 1003, 1005]))
-    expect(app.findChildren(1001, arr)).not.toEqual(expect.arrayContaining([1001, 1004]))
-    expect(app.findChildren(1001, arr).length).toBe(3)
+    expect(app.findChildren(1001, data)).toEqual(expect.arrayContaining([1002, 1003, 1005]))
+    expect(app.findChildren(1001, data)).not.toEqual(expect.arrayContaining([1001, 1004]))
+    expect(app.findChildren(1001, data).length).toBe(3)
   })
 })
 
 describe('insertChildren', () => {
-  const arr = [
+  const data = [
     {
       categoryId: 1001,
       parent: 'root'
@@ -59,13 +55,80 @@ describe('insertChildren', () => {
   ]
 
   test('categories with children have array with values', () => {
-    const newArr = app.insertChildren(arr)
-    expect(newArr.length).toBe(5)
-    expect(newArr[0].children).toEqual(expect.arrayContaining([1002, 1003, 1005]))
+    const arr = app.insertChildren(data)
+    expect(arr.length).toBe(5)
+    expect(arr[0].children).toEqual(expect.arrayContaining([1002, 1003, 1005]))
   })
 
   test('categories without children have empty array', () => {
-    const newArr = app.insertChildren(arr)
-    expect(newArr[4].children.length).toBe(0)
+    const arr = app.insertChildren(data)
+    expect(arr[4].children.length).toBe(0)
+  })
+})
+
+describe('buildCsvData', () => {
+  const data = [
+    {
+      categoryId: 'root',
+      'display-name': 'root',
+      children: [1001],
+      parent: ''
+    },
+    {
+      categoryId: 1001,
+      'display-name': 'fruit',
+      children: [1002, 1003, 1005],
+      parent: 'root'
+    },
+    {
+      categoryId: 1002,
+      'display-name': 'apple',
+      children: [1004],
+      parent: '1001'
+    },
+    {
+      categoryId: 1003,
+      'display-name': 'grapes',
+      children: [],
+      parent: '1001'
+    },
+    {
+      categoryId: 1004,
+      'display-name': 'granny smith',
+      children: [1006],
+      parent: '1002'
+    },
+    {
+      categoryId: 1005,
+      'display-name': 'kiwifruit',
+      children: [],
+      parent: '1001'
+    },
+    {
+      categoryId: 1006,
+      'display-name': 'organic',
+      children: [],
+      parent: '1004'
+    }
+  ]
+  test('returns and array with 7 arrays nested init', () => {
+    const arr = app.buildCsvData(data[0], data)
+    expect(arr.length).toBe(7)
+    expect(Array.isArray(arr[0])).toBeTruthy()
+    expect(Array.isArray(arr[1])).toBeTruthy()
+    expect(Array.isArray(arr[2])).toBeTruthy()
+    expect(Array.isArray(arr[3])).toBeTruthy()
+    expect(Array.isArray(arr[4])).toBeTruthy()
+    expect(Array.isArray(arr[5])).toBeTruthy()
+    expect(Array.isArray(arr[6])).toBeTruthy()
+  })
+
+  test('array should be ready to convert to CSV', () => {
+    const arr = app.buildCsvData(data[0], data)
+    expect(arr[0]).toEqual(expect.arrayContaining(['name', 'id','level 1', 'level 2', 'level 3', 'level 4']))
+    expect(arr[1]).toEqual(expect.arrayContaining(['fruit', 1001, 'root', 1001]))
+    expect(arr[2]).toEqual(expect.arrayContaining(['apple', 1002, 'root', 1001, 1002]))
+    expect(arr[3]).toEqual(expect.arrayContaining(['granny smith', 1004, 'root', 1001, 1002, 1004]))
+    expect(arr[4]).toEqual(expect.arrayContaining(['organic', 1006, 'root', 1001, 1002, 1004, 1006]))
   })
 })
