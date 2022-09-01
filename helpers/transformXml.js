338b8446dc32943414c4ff2dfe3bb2cbf0039f1b
Jared <jared@needapart.co.nz>
2022-09-01 21:59:35 +1200

    Parse SFCC XML return categories with CGID, name, part, online status

--- helpers/transformXml.js
+++ helpers/transformXml.js
@@ -1,3 +1,31 @@
-// read xml
+const fs = require('fs')
+const path = require('path')
+const xml2js = require('xml2js')
+const parser = new xml2js.Parser()
+const util = require('util')
 
-// convert xml to js object
+const filePath = path.join(__dirname, './../data/twl-storefront-20220824.xml')
+
+let catalog = new Object
+
+const categories = fs.readFile(filePath, (err, data) => {
+  parser.parseStringPromise(data)
+    .then((result) => { return result.catalog.category })
+    .then((result) => {
+      const categories = new Array
+      for (let i = 0; i < result.length; i++) {
+        const data = result[i]
+        const category = new Object
+        // extract required attributes from data
+        category.cgid = data.$['category-id']
+        category.name = data['display-name'][0]._
+        category.parent = data.parent === undefined ? null : data.parent[0]
+        category.online = data['online-flag'][0]
+        // add category to array
+        categories.push(category)
+      }
+      console.dir(categories)
+    })
+    .catch((err) => console.error(err))
+})
+
