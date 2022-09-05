c5b6b059c4693b13b1793cb297d191c03510e642
Jared <jared@needapart.co.nz>
2022-09-05 23:39:28 +1200

    Rewrite transformXml to return a promise and draft tests

--- helpers/transformXml.js
+++ helpers/transformXml.js
@@ -1,30 +1,37 @@
 const fs = require('fs')
-const path = require('path')
 const xml2js = require('xml2js')
 const parser = new xml2js.Parser()
 
-const filePath = path.join(__dirname, './../data/twl-storefront-20220824.xml')
-
-let catalog = new Object
-
-const categories = fs.readFile(filePath, (err, data) => {
-  parser.parseStringPromise(data)
-    .then((result) => { return result.catalog.category })
-    .then((result) => {
-      const categories = new Array
-      for (let i = 0; i < result.length; i++) {
-        const data = result[i]
-        const category = new Object
-        // extract required attributes from data
-        category.cgid = data.$['category-id']
-        category.name = data['display-name'][0]._
-        category.parent = data.parent === undefined ? null : data.parent[0]
-        category.online = data['online-flag'][0]
-        // add category to array
-        categories.push(category)
-      }
-      console.dir(categories)
+const transformXml = (filePath) => {
+  return new Promise ( res => {
+    const categories = fs.readFile(filePath, (err, data) => {
+      parser.parseStringPromise(data)
+        .then((result) => { return result.catalog.category })
+        .then((result) => {
+          const categories = new Array
+          for (let i = 0; i < result.length; i++) {
+            const data = result[i]
+            const category = new Object
+            // extract required attributes from data
+            category.cgid = data.$['category-id']
+            category.name = data['display-name'][0]._
+            category.parent = data.parent === undefined ? null : data.parent[0]
+            category.online = data['online-flag'][0]
+            // add category to array
+            categories.push(category)
+          }
+          res(categories)
+        })
+        .catch((err) => console.error(err))
     })
-    .catch((err) => console.error(err))
-})
+  })
+}
 
+// const path = require('path')
+// const filePath = path.join(__dirname, './../data/twl-storefront-20220824.xml')
+// transformXml(filePath)
+//   .then((res) => {
+//     console.dir(res)
+//   })
+
+module.exports = { transformXml }
---
+++ test/transformXml.test.js
@@ -0,0 +1,14 @@
+const { transformXml } = require("../helpers/transformXml");
+
+// Tests for transformXml
+describe('transformXML', () => {
+  // test('should return a promise', () ={
+
+  // })
+
+  // test('transformXML should error in ...', () => {})
+
+  // test('return an array with nested objects', () => {})
+  // test('nested objects have 4 properties', () => {})
+  
+})
