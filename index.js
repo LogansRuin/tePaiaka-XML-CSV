d15d0b8b089259dff39ba51720c986d4a167dd53
Jared <jared@needapart.co.nz>
2022-10-03 21:05:51 +1300

    new dummy data

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const { insertChildren, buildCsvData, writeCsvFile } = require('./helpers/csvBui
 const { transformXml } = require('./helpers/transformXml')
 const path = require('path')
 
-const xmlPath = path.join(__dirname, '/data/wsl-nav-20220822.xml')
-// const json = require('./data/twlcat.json')
+const xmlPath = path.join(__dirname, './data/twl-storefront-20221003.xml')
 
 function app (filePath) {
   transformXml(filePath).then(res => {
     const arr = insertChildren(res)
     const root = arr[0]
     const data = buildCsvData(root, arr)
-    writeCsvFile(data)
+    writeCsvFile(data, 'TWL-Navigation-')
   })
   
 }
