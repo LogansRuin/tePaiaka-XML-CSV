f15f96ded41d92108db93f96c4c37b7a945aedfa
Jared <jared@needapart.co.nz>
2022-10-04 23:05:00 +1300

    /category route receives an xml file and saves it to data > uploads

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
+
+const upload = multer({ 
+  dest: 'data/uploads',
+  fileFilter: (req, file, callback) => {
+    const ext = path.extname(file.originalname)
+    if (ext !== ".xml") {
+      return callback( new Error('File format must be application/xml'))
+    }
+    callback(null, true)
+  }
+})
 
 router.get('/', (req, res, next) => {
   res.json({
     msg: "router is working"
+  })
+})
+
+router.post('/category', upload.single('uploaded_file'), (req, res, next) => {
+  const filename = `${req.body.filename}-${Date.now()}.xml`
+  const oldPath = req.file.path
+  const newPath = `${req.file.destination}/${filename}`
+  fs.rename( oldPath, newPath, (err) => {
+    if (err) throw err
+    res.json({
+      success: true,
+      msg: "File writen",
+      path: newPath,
+      feilds: req.body,
+      file: req.file
+    })
   })
 })
 
